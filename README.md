71b83273c61e4024441b94e901ede5a4bc5752db
Pieter Smit <pieter.smit@twgroup.co.nz>
2023-09-04 09:10:21 +1200

    fix: add static /s endpoint

--- README.md
+++ README.md
@@ -1,9 +1,31 @@
 # rust-htmx-play
 
+## History
+
+* 2023-09-04 PESmit - create initial files using axum as it integrates well with tokio
+
+## ToDo
+
+* [] Remove tower - not used
+* [] Remove serve_dir.rs, replaced with in binary static /s
+
+## http endpoints
+
+* /s/ has static files loaded into the binary !
+* /static/ loads files dynamically from ./static/
+* /hello/:name responds with :name
+
 ## Debug run
 
       cargo install cargo-watch
       cargo watch -x run
+
+      # In 2nd terminal
+      curl -i http://127.0.0.1:8080/
+      curl -i http://127.0.0.1:8080/s/index.html
+      curl -i http://127.0.0.1:8080/hello/testing
+      curl -i http://127.0.0.1:8080/static/hello2.html
+
 
 
 
--- shop/src/main.rs
+++ shop/src/main.rs
@@ -2,20 +2,19 @@
 // use actix_web::{get, web, App, HttpServer, Responder};
 use anyhow::Result;
 use axum::{
-    routing::{get, post},
     http::StatusCode,
     response::IntoResponse,
-    Json, Router};
-
-use std::net::SocketAddr;
+    routing::{get, get_service, post},
+    Json, Router, ServiceExt,
+};
 use serde::{Deserialize, Serialize};
+use tower::ServiceBuilder;
 
-//#[get("/")]
 async fn index() -> impl IntoResponse {
     "Hello, World! from actix"
 }
 
-async fn hello(name: axum::extract::Path<String> ) -> impl IntoResponse {
+async fn hello(name: axum::extract::Path<String>) -> impl IntoResponse {
     let n: String = name.to_string();
     format!("Hello {}!\n", n)
 }
@@ -30,17 +29,59 @@ async fn main() -> std::io::Result<()> {
     println!("Next step ..");
     Ok(())
 }
-//#[actix_web::main]
+
+
 async fn start_webserver(webserver_ip: &str, webserver_port: u16) -> Result<()> {
+    //let s_dir = tower_http::services::ServeDir::new("static");
+
     let app = Router::new()
         .route("/", get(index))
-        .route("/hello/:name", get(hello));
+        .route("/hello/:name", get(hello))
+        .route("/s/*path", get(static_path))
+        .route(
+            "/static/*path",
+            get_service(tower_http::services::ServeDir::new("").fallback(
+                tower_http::services::ServeFile::new("static/not_found.html"),
+            ))
+            .handle_error(|error| async move {
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Unhandled internal error: {}", error),
+                )
+            }),
+        );
 
-    let addr = format!("{}:{}",webserver_ip,webserver_port).parse().expect("Invalid IP Addr");
+    let addr = format!("{}:{}", webserver_ip, webserver_port)
+        .parse()
+        .expect("Invalid IP Addr");
     // tracing::info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
     Ok(())
+}
+
+use include_dir::{include_dir, Dir};
+static STATIC_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/static"); // embedding an entire directory tree into your binary
+async fn static_path(path: axum::extract::Path<String>) -> impl IntoResponse {
+    let mut path = path.trim_start_matches('/');
+    if path.len() == 0 { path = "index.html"; }
+    let mime_type = mime_guess::from_path(path).first_or_text_plain();
+    println!("path={path}  {mime_type}");
+
+    match STATIC_DIR.get_file(path) {
+        None => axum::response::Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(axum::body::boxed(axum::body::Empty::new()))
+            .unwrap(),
+        Some(file) => axum::response::Response::builder()
+            .status(StatusCode::OK)
+            .header(
+                axum::http::header::CONTENT_TYPE,
+                axum::http::HeaderValue::from_str(mime_type.as_ref()).unwrap(),
+            )
+            .body(axum::body::boxed(axum::body::Full::from(file.contents())))
+            .unwrap(),
+    }
 }
